ce7777ca6f8dcb3acc58eaf40f8b48eee774f792
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2021-12-19 21:06:25 -0300

    Agregar archivos de proyecto.

---
+++ De cinco en cinco/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_cinco_en_cinco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i = i + 5)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
